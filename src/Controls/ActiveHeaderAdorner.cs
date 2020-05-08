295752b14052b43df33ffa22ea1adc401a9c79a0
yzh <yangzhenghan@lonwin.org>
2020-05-08 17:27:28 +0800

    修改软件图标

---
+++ src/Controls/ActiveHeaderAdorner.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+using System.Windows.Documents;
+using System.Windows.Media;
+
+namespace SimpleDICOMToolkit.Controls
+{
+    public class ActiveHeaderAdorner : Adorner
+    {
+        public ActiveHeaderAdorner(UIElement adornedElement) : base(adornedElement)
+        {
+        }
+
+        protected override void OnRender(DrawingContext drawingContext)
+        {
+            Rect adornedElementRect = new Rect(this.AdornedElement.DesiredSize);
+
+            SolidColorBrush renderBrush = new SolidColorBrush(Color.FromRgb(0xf0, 0x5b, 0x72)) { Opacity = 0.8 };
+            Pen renderPen = new Pen(renderBrush, 0);
+
+            drawingContext.DrawEllipse(renderBrush, renderPen, adornedElementRect.TopRight, 6, 6);
+        }
+    }
+}
---
+++ src/Utils/VisualTreeUtil.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Media;
+
+namespace SimpleDICOMToolkit.Utils
+{
+    public static class VisualTreeUtil
+    {
+        public static FrameworkElement GetDescendantFromName(DependencyObject parent, string name)
+        {
+            int count = VisualTreeHelper.GetChildrenCount(parent);
+
+            if (count < 1)
+            {
+                return null;
+            }
+
+            for (int i = 0; i < count; i++)
+            {
+                if (VisualTreeHelper.GetChild(parent, i) is FrameworkElement frameworkElement)
+                {
+                    if (frameworkElement.Name == name)
+                    {
+                        return frameworkElement;
+                    }
+
+                    frameworkElement = GetDescendantFromName(frameworkElement, name);
+
+                    if (frameworkElement != null)
+                    {
+                        return frameworkElement;
+                    }
+                }
+            }
+            return null;
+        }
+
+        /// <summary>
+        /// 利用visualtreehelper寻找对象的子级对象
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public static List<T> FindVisualChild<T>(DependencyObject obj) where T : FrameworkElement
+        {
+            int count = VisualTreeHelper.GetChildrenCount(obj);
+
+            List<T> TList = new List<T>();
+            for (int i = 0; i < count; i++)
+            {
+                DependencyObject child = VisualTreeHelper.GetChild(obj, i);
+
+                if (child != null && child is T)
+                {
+                    TList.Add((T)child);
+                }
+                else
+                {
+                    List<T> childOfChildren = FindVisualChild<T>(child);
+                    if (childOfChildren != null)
+                    {
+                        TList.AddRange(childOfChildren);
+                    }
+                }
+            }
+
+            return TList;
+        }
+
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public static List<T> FindVisualParent<T>(DependencyObject obj) where T : FrameworkElement
+        {
+            List<T> TList = new List<T> { };
+
+            DependencyObject parent = VisualTreeHelper.GetParent(obj);
+            if (parent != null && parent is T)
+            {
+                TList.Add((T)parent);
+                List<T> parentOfParent = FindVisualParent<T>(parent);
+                if (parentOfParent != null)
+                {
+                    TList.AddRange(parentOfParent);
+                }
+            }
+
+            return TList;
+        }
+    }
+}
