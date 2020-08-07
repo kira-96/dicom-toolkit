e5e302c3cb84f13c3968019520ad028c6588f090
kira <kirakira@makino.house>
2020-08-07 19:43:41 +0800

    :arrow_up: 升级依赖包

--- src/Initializer.cs
+++ src/Initializer.cs
@@ -10,6 +10,10 @@
 
             Dicom.Imaging.ImageManager.SetImplementation(Dicom.Imaging.WPFImageManager.Instance);
             Dicom.Log.LogManager.SetImplementation(Dicom.Log.NLogManager.Instance);
+
+            // Set your own Class UID here
+            // Dicom.DicomImplementation.ClassUID = new Dicom.DicomUID("My Class UID", "Implementation Class UID", Dicom.DicomUidType.Unknown);
+            // Dicom.DicomImplementation.Version = "My Version Name";
         }
     }
 }
