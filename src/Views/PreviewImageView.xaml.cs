ae9b96cd456a70187e662e5c3bd7141152328a15
kira-96 <847320916@QQ.com>
2021-01-04 11:02:52 +0800

    :new: 图像窗口新增全屏快捷键

--- src/Views/PreviewImageView.xaml.cs
+++ src/Views/PreviewImageView.xaml.cs
@@ -1,13 +1,67 @@
-﻿namespace SimpleDICOMToolkit.Views
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace SimpleDICOMToolkit.Views
 {
     /// <summary>
     /// PreviewImageView.xaml 的交互逻辑
     /// </summary>
     public partial class PreviewImageView
     {
+        private bool isFullscreen = false;
+        private bool isTopmost = false;
+        private Rect normalWindowRect;
+        private WindowState normalState = WindowState.Normal;
+        private WindowStyle normalStyle = WindowStyle.SingleBorderWindow;
+        private ResizeMode normalResizeMode = ResizeMode.CanResize;
+
         public PreviewImageView()
         {
             InitializeComponent();
+
+            CommandBindings.Add(new CommandBinding(WindowCommands.FullscreenCommand, ToggleFullscreen));
         }
+
+        private void ToggleFullscreen(object sender, ExecutedRoutedEventArgs e)
+        {
+            if (!isFullscreen)
+            {
+                // backup state
+                isTopmost = this.Topmost;
+                normalState = this.WindowState;
+                normalStyle = this.WindowStyle;
+                normalResizeMode = this.ResizeMode;
+                normalWindowRect = new Rect(Left, Top, ActualWidth, ActualHeight);
+
+                // enter fullscreen
+                this.Topmost = true;
+                this.WindowState = WindowState.Normal;
+                this.WindowStyle = WindowStyle.None;
+                this.ResizeMode = ResizeMode.NoResize;
+                this.Left = SystemParameters.WorkArea.Left;
+                this.Top = SystemParameters.WorkArea.Top;
+                this.Width = SystemParameters.WorkArea.Width;
+                this.Height = SystemParameters.WorkArea.Height;
+                isFullscreen = true;
+            }
+            else
+            {
+                // exit fullscreen
+                this.Topmost = isTopmost;
+                this.WindowState = normalState;
+                this.WindowStyle = normalStyle;
+                this.ResizeMode = normalResizeMode;
+                this.Left = normalWindowRect.Left;
+                this.Top = normalWindowRect.Top;
+                this.Width = normalWindowRect.Width;
+                this.Height = normalWindowRect.Height;
+                isFullscreen = false;
+            }
+        }
+    }
+
+    public class WindowCommands
+    {
+        public static RoutedUICommand FullscreenCommand { get; } = new RoutedUICommand();
     }
 }
