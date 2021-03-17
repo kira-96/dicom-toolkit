72461c521f179ab7f7292c1aea51a84db5640252
kira-96 <847320916@QQ.com>
2021-03-17 15:08:52 +0800

    rename: BoolInvertConverter -> BoolNegationConverter

--- src/Converters/BoolNegationConverter.cs
+++ src/Converters/BoolNegationConverter.cs
@@ -4,16 +4,16 @@ using System.Windows.Data;
 
 namespace SimpleDICOMToolkit.Converters
 {
-    public class BoolInvertConverter : IValueConverter
+    public class BoolNegationConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            return !(bool)value;
+            return !(value is bool boolean && boolean);
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            return !(bool)value;
+            return !(value is bool boolean && boolean);
         }
     }
 }
